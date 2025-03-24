6ac30f7da9284d01feedc335f50fbacfa2ac869a
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-03-24 13:16:19 +0000

    Mudei os caracteres

---
+++ NivelNovato.c/NivelNovato.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  int main(){
+    char estado;
+    char CodigoDaCarta[4], NomeDaCidade[20];
+    int populaçao, NumeroDePontosTuristicos;
+    float área, PIB;
+    
+      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04.");
+        
+  }
