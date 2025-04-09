f487631e90ab2bcfef6ecfc425efac6296fbdb2e
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-04-09 13:27:23 +0000

    Scanf adicionado

--- NivelNovato.c/NivelNovato.c
+++ NivelNovato.c/NivelNovato.c
@@ -2,10 +2,23 @@
   int main(){
     char estado;
     char CodigoDaCarta[4], NomeDaCidade[20];
-    int populacao;
+    int Populacao;
     int PontosTuristicos;
     float Area, PIB;
     
-      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas\n");
-      printf("");
+      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas, Depois escolha uma cidade, imforme o número de habitantes da cidade, imforme a área da cidade, imforme o PIB, imforme o Número de pontos turísticos.\n");
+      printf("Escolha seu estado: ");
+      scanf("%c", &estado);
+      
+      printf("Escolha o Código da carta: ");
+      scanf("%f", &CodigoDaCarta);
+
+      printf("Imforme o nome da cidade: ");
+      scanf("%s", &NomeDaCidade);
+
+      printf("Imforme o número de Habitantes da cidade: ");
+      scanf("%f", &Populacao);
+
+      printf("Imforme a Área da cidade: ");
+      scanf("%e", &Area);
     }
