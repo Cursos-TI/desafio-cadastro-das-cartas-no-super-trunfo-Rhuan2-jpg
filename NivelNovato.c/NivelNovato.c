d73437e96f7b6896c22f6af596d494d6d4899f0e
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-04-09 12:47:28 +0000

    escrevi mais no printf

--- NivelNovato.c/NivelNovato.c
+++ NivelNovato.c/NivelNovato.c
@@ -2,9 +2,10 @@
   int main(){
     char estado;
     char CodigoDaCarta[4], NomeDaCidade[20];
-    int populaçao, NumeroDePontosTuristicos;
-    float área, PIB;
+    int populacao;
+    int PontosTuristicos;
+    float Area, PIB;
     
-      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04.");
-        
-  }
+      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas\n");
+      printf("");
+    }
