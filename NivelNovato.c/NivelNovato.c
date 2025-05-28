f0f699a2d30dc45c1952164a657c82e44a131f58
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-05-28 12:22:27 +0000

    melhorei +++

--- NivelNovato.c/NivelNovato.c
+++ NivelNovato.c/NivelNovato.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
   int main(){
     char estado;
-    char CodigoDaCarta[4], NomeDaCidade[12];
+    char CodigoDaCarta[4], NomeDaCidade[25];
     int Populacao;
     int PontosTuristicos;
     float area, pib;
+    int temp_char;
     
       printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas, Depois escolha uma cidade, imforme o número de habitantes da cidade, imforme a área da cidade, imforme o PIB, imforme o Número de pontos turísticos.\n");
       printf("Escolha seu estado: ");
-      scanf("%c", &estado);
+      scanf(" %c", &estado);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
       
       printf("Escolha o Código da carta: ");
-      scanf("%i", &CodigoDaCarta);
+      scanf("%3s", CodigoDaCarta);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
       printf("Imforme o nome da cidade: ");
-      scanf("%s", NomeDaCidade);
+      scanf("%24s", NomeDaCidade);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
       printf("Imforme o número de Habitantes da cidade: ");
       scanf("%d", &Populacao);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
       printf("Imforme a Área da cidade: ");
-      scanf("%.2f", &area);
+      scanf("%f", &area);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
       printf("Imforme o PIB: ");
-      scanf("%.2f", &pib);
+      scanf("%f", &pib);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
       printf("Imforme o Número de Pontos Turísticos: ");
       scanf("%d", &PontosTuristicos);
+      while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("as imformações da sua carta são estas começando pelo estado: %c", &estado);
+      printf("as imformações da sua carta são estas respectivamente: %c, %3s, %24s, %d, %f, %f, %d", &estado, CodigoDaCarta, NomeDaCidade, &Populacao, &area, &pib, &PontosTuristicos);
     
     
     
