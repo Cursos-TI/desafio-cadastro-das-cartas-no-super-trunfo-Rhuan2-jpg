333a682c394171444eb9b7308477e85f99a1be5b
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-06-02 12:53:56 +0000

    km² colocado

--- NivelNovato.c/NivelNovato.c
+++ NivelNovato.c/NivelNovato.c
@@ -8,7 +8,7 @@
     float area, pib;
     int temp_char;
        //Explicação do que deve ser inserido na primeira carta
-      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas, Depois escolha uma cidade, imforme o número de habitantes da cidade, imforme a área da cidade, imforme o PIB, imforme o Número de pontos turísticos.\n");
+      printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas, Depois escolha uma cidade, informe o número de habitantes da cidade, informe a área da cidade, informe o PIB, informe o Número de pontos turísticos.\n");
       printf("Escolha seu estado: ");
       scanf(" %c", &estado);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
@@ -17,27 +17,27 @@
       scanf("%3s", CodigoDaCarta);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o nome da cidade: ");
+      printf("Informe o nome da cidade: ");
       scanf("%24s", NomeDaCidade);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o número de Habitantes da cidade: ");
+      printf("Informe o número de Habitantes da cidade: ");
       scanf("%d", &Populacao);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme a Área da cidade: ");
+      printf("Informe a Área da cidade: ");
       scanf("%f", &area);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o PIB: ");
+      printf("Informe o PIB: ");
       scanf("%f", &pib);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o Número de Pontos Turísticos: ");
+      printf("Informe o Número de Pontos Turísticos: ");
       scanf("%d", &PontosTuristicos);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
         //Informações da primeira carta juntas
-       printf("as imformações da sua carta são estas respectivamente: %c, %s, %s, %d, %.2f, %.2f, %d\n", estado, CodigoDaCarta, NomeDaCidade, Populacao, area, pib, PontosTuristicos);
+       printf("as informações da sua carta são estas respectivamente: %c, %s, %s, %d, %.2f km², %.2f, %d\n", estado, CodigoDaCarta, NomeDaCidade, Populacao, area, pib, PontosTuristicos);
         //Dados e váriaveis para segunda carta
     char Estado;
     char codigoDaCarta[4],nomeDaCidade[25]; 
@@ -53,27 +53,27 @@
       scanf("%3s", codigoDaCarta);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o nome da cidade: ");
+      printf("Informe o nome da cidade: ");
       scanf("%24s", nomeDaCidade);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o número de Habitantes da cidade: ");
+      printf("Informe o número de Habitantes da cidade: ");
       scanf("%d", &populacao);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme a Área da cidade: ");
+      printf("Informe a Área da cidade: ");
       scanf("%f", &Area);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o PIB: ");
+      printf("Informe o PIB: ");
       scanf("%f", &Pib);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
 
-      printf("Imforme o Número de Pontos Turísticos: ");
+      printf("Informe o Número de Pontos Turísticos: ");
       scanf("%d", &pontosTuristicos);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
-        //imformações da segunda carta juntas
-       printf("as imformações da sua carta são estas respectivamente: %c, %s, %s, %d, %f, %.2f, %d\n", Estado, codigoDaCarta, nomeDaCidade, populacao, Area, Pib, pontosTuristicos);
+        //Informações da segunda carta juntas
+       printf("as informações da sua carta são estas respectivamente: %c, %s, %s, %d, %.2f km², %.2f, %d\n", Estado, codigoDaCarta, nomeDaCidade, populacao, Area, Pib, pontosTuristicos);
     
     
     }
